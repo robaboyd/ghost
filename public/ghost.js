781f79369a57c7d3f335f2d21a1ea555829b2288
Bobby Boyd <bboyd2008@gmail.com>
2019-07-12 16:32:42 -0700

    weather update

--- public/ghost.js
+++ public/ghost.js
@@ -3,42 +3,44 @@ let p = document.createElement('p')
 const words = document.querySelector('.transcript')
 
 //socket
-var socket = io('http://🤖:3001');
+// var socket = io('http://🤖:3001');
+var socket = io('http://localhost:3001');
 
 
-socket.on('connect', function(){
+
+socket.on('connect', function () {
 	console.log('connected');
 	//get weather
-	
+
 	//speech recognition
 	window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-	
+
 	var Omega = new SpeechSynthesisUtterance();
-	
+
 	const recognition = new SpeechRecognition();
 
 	recognition.interimResults = true;
-	
+
 	let listening = false;
 	let names = ""
 
 	var siriWave = new SiriWave({
 		container: document.getElementById('siri-container'),
 		style: "ios9",
-		autostart: true, 
+		autostart: true,
 		width: 440,
 		height: 150,
-		amplitude:0.1,
+		amplitude: 0.1,
 		speed: .1
 	});
-	
+
 	recognition.addEventListener('result', e => {
 		console.log(listening);
 
 		let transcript = Array.from(e.results)
-		.map(result => result[0])
-		.map(result => result.transcript)
-		.join('')
+			.map(result => result[0])
+			.map(result => result.transcript)
+			.join('')
 
 		transcript = transcript.toLowerCase();
 
@@ -51,22 +53,22 @@ socket.on('connect', function(){
 		// socket.on('showCommand', data=> {
 		// 	showText(transcript)
 		// })
-		
+
 		//if the transcript is just omega
-		if(listening === true  || transcript.includes('omega')) {
+		if (listening === true || transcript.includes('omega')) {
 			$('.wrapper').addClass('listening');
-			socket.emit('lightActivate', null)	
+			socket.emit('lightActivate', null)
 			//send messages to the server to run the scripts
-			if(e.results[0].isFinal){
+			if (e.results[0].isFinal) {
 				socket.emit('commandSent', transcript)
 			}
-		}	
-		if(transcript === 'omega'){
+		}
+		if (transcript === 'omega') {
 			console.log('just omega');
 			listening = true
-			if(e.results[0].isFinal){
+			if (e.results[0].isFinal) {
 				$('.wrapper').addClass('listening');
-				socket.emit('lightActivate', null)	
+				socket.emit('lightActivate', null)
 				response(`yes, what do you need ${names}`, 1.5)
 			}
 		}
@@ -74,75 +76,75 @@ socket.on('connect', function(){
 		socket.on('commandDone', data => {
 			//end
 			console.log('command done');
-			$('.wrapper').removeClass('listening')	
-				socket.emit('lightsDeactivate', null)
-				// if(data !== null){
-				// 	listening = false
-				// } else {
-				// 	response('nevermind then', 1.0)
-					
-				// }
-				if(data !== null){
+			$('.wrapper').removeClass('listening')
+			socket.emit('lightsDeactivate', null)
+			// if(data !== null){
+			// 	listening = false
+			// } else {
+			// 	response('nevermind then', 1.0)
 
-					response(data.speech, 1.0)
-				}
-				listening = false
+			// }
+			if (data !== null) {
+
+				response(data.speech, 1.0)
+			}
+			listening = false
 		})
 
 		//on power run
 		socket.on('powerRun', (data) => {
 			console.log(`🤖 ${JSON.stringify(data)}`);
 		})
-			
-			if(transcript.includes('stop')){
-				if(e.results[0].isFinal){
-					speechSynthesis.cancel()
-					$('.wrapper').removeClass('listening')	
-					listening = false
-					console.log(`not listening `);
-				}
-			}
 
-			if(e.results[0].isFinal){
-				console.log(transcript);
+		if (transcript.includes('stop')) {
+			if (e.results[0].isFinal) {
+				speechSynthesis.cancel()
+				$('.wrapper').removeClass('listening')
+				listening = false
+				console.log(`not listening `);
 			}
-		
-		})
-		
-		const response = (text, rate) => {
+		}
+
+		if (e.results[0].isFinal) {
+			console.log(transcript);
+		}
+
+	})
+
+	const response = (text, rate) => {
 		console.log(`Speaking... ${text}`);
 		siriWave.amplitude = 2
 		siriWave.speed = .4
-		
+
 		Omega.text = text;
 		Omega.lang = 'en-US';
 		Omega.rate = rate;
-		Omega.onend = function(event) {
+		Omega.onend = function (event) {
 			siriWave.amplitude = 0.1
 			siriWave.speed = .1
-			speechSynthesis.cancel() 
+			speechSynthesis.cancel()
 		}
-		
+
 		speechSynthesis.speak(Omega);
-		
+
 		hideText()
 	}
-	
+
 	//get the user name
 
 	const showText = (transcript) => {
-		p.textContent= transcript
+		p.textContent = transcript
 		words.appendChild(p)
-		$('.transcript').css({opacity: 1, transition: 'all .5s ease'})
+		$('.transcript').css({ opacity: 1, transition: 'all .5s ease' })
 	}
-	
+
 	const hideText = () => {
 		setTimeout(() => {
 			p.textContent = ''
-			$('.transcript').css({opacity: 1, transition: 'all .5s ease'})
+			$('.transcript').css({ opacity: 1, transition: 'all .5s ease' })
 		}, 2000)
 	}
 	recognition.addEventListener('end', recognition.start)
 	recognition.start();
-	
+
 });
--- public/weather.js
+++ public/weather.js
@@ -4,21 +4,27 @@ let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiK
 let weatherP = document.getElementById('weather')
 
 const weather = () => {
-   $.ajax({
-     type: 'GET', 
-     url: url,
-     dataType: "json",
-     success: function(result, status, xhr){
-       console.log(result.weather);
-       if(result.weather[0].description === "clear sky"){
-
-         weatherP.textContent =   `🔆 ${city} ${result.main.temp.toString().split('.')[0]}°F `
-       } else {
-         weatherP.textContent = result.main.temp.toString().split('.')[0] + '°F'
-        }
-     }
-   })
-  }
-  weather()
-  setInterval(weather, 60000);
+  $.ajax({
+    type: 'GET',
+    url: url,
+    dataType: "json",
+    success: function (result, status, xhr) {
+      console.log(result.weather[0].main);
+      let temp = result.main.temp.toString().split('.')[0]
+      if (parseInt(temp) > 90) {
+        weatherP.textContent = `😰 ${city} ${result.main.temp.toString().split('.')[0]}°F `
+      } else if (parseInt(temp) > 80 && result.weather[0].main !== "Rain") {
+        weatherP.textContent = `🌤 ${city} ${result.main.temp.toString().split('.')[0]}°F `
+      } else if (parseInt(temp) > 70 && result.weather[0].main !== "Rain") {
+        weatherP.textContent = `😎 ${city} ${result.main.temp.toString().split('.')[0]}°F `
+      } else if (parseInt(temp) > 50 && parseInt(temp) < 70) {
+        weatherP.textContent = `☁ ${city} ${result.main.temp.toString().split('.')[0]}°F `
+      } else if (parseInt(temp) < 50 && result.weather[0].main !== "Rain") {
+        weatherP.textContent = `❄ ${city} ${result.main.temp.toString().split('.')[0]}°F `
+      }
+    }
+  })
+}
+weather()
+setInterval(weather, 60000);
 
